890d2a468c473d7dddf73c4a0bd827ba98ac5d0b
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-04-13 01:41:32 +0000

    crm tables

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,20 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+create table contacts (
+contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name text,
+last_name text,
+email text,
+phone text
+);
+
+create table activities (
+activity_id INTEGER PRIMARY KEY AUTOINCREMENT,
+contact_id INTEGER,
+date text,
+time text,
+activity_type text,
+notes text
+);
